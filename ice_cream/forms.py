de923e937fc052ab48ad17f3248a49bba92de13a
Alexander Meyer <alexm@mighty.com>
2016-06-08 11:55:31 -0400

    Check customer email and send order information to it

--- ice_cream/forms.py
+++ ice_cream/forms.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 
 
 class OrderForm(forms.ModelForm):
-
     flavor = forms.ModelChoiceField(queryset=Flavor.objects.all())
     toppings = forms.ModelMultipleChoiceField(queryset=Topping.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
     container = forms.ModelChoiceField(queryset=Container.objects.all())
+    email = forms.EmailField()
 
     class Meta:
         model = Order
--- ice_cream/views.py
+++ ice_cream/views.py
@@ -6,48 +6,57 @@ from .models import Flavor, Topping, Container
 from django.core.mail import send_mail
 
 
-def home_page(request):
-    return render(request, 'ice_cream/home_page.html', {})
+def send_email_to_claire(body, name):
+    email_body = 'A new order for ice cream was made by ' + name + '.\n\n' + \
+                body
+    send_mail('New Order',
+              email_body,
+              'claires.icecream.order@gmail.com',
+              ['meyer.alexander.john@gmail.com'],
+              fail_silently=False
+              )
+
+
+def send_email_to_customer(body, name, to_email):
+    email_body = name + ',\n\n' + "Thank you for ordering from Claire's Creamery! Your order details are as follows:" + \
+                '\n\n' + body + '\n\n' + "Let us know if there are any issues, but mostly, enjoy that " + \
+                "ice cream!\n\nYour sweet tooth savior,\nClaire's Creamery"
+    send_mail('Your Order',
+              email_body,
+              'claires.icecream.order@gmail.com',
+              [to_email],
+              fail_silently=False
+              )
 
 
 def orderview(request):
-    """
-    template_name = 'order.html'
-    form_class = OrderForm
-    success_url = '/thanks/'
 
-    """
+    def make_order_body():
+        toppings_str = ''
+
+        for topping in form.cleaned_data.get('toppings'):
+            toppings_str += str(topping) + ', '
+        toppings_str = toppings_str[:-2]
+
+        order_body = 'Flavor: ' + order.flavor + '\n' + \
+                     'Container: ' + order.container
+        # if not empty, show toppings
+        if len(toppings_str) > 0:
+            order_body += '\n' + 'Toppings: ' + toppings_str
+
+        return order_body
+
     form = OrderForm(request.POST or None)
 
     if request.method == "POST":
-
         if form.is_valid():
             order = form.save(commit=False)
             order.order_time = datetime.datetime.now()
             order.save()
 
-            toppings_str = ''
-
-            for topping in form.cleaned_data.get('toppings'):
-                toppings_str += str(topping) + ', '
-
-            # get rid of final comma
-            toppings_str = toppings_str[:-2]
-
-            email_body = 'A new order for ice cream was made by ' + order.name + '.\n\n' + \
-                         'Flavor: ' + order.flavor + '\n' + \
-                         'Container: ' + order.container + '\n'
-
-            # if not empty, show toppings
-            if len(toppings_str) > 0:
-                email_body += 'Toppings: ' + toppings_str
-
-            send_mail('New Order',
-                      email_body,
-                      'claires.icecream.order@gmail.com',
-                      ['meyer.alexander.john@gmail.com'],
-                      fail_silently=False
-                    )
+            body = make_order_body()
+            send_email_to_claire(body, order.name)
+            send_email_to_customer(body, order.name, order.email)
 
             # once ordered, redirect
             return redirect('success/')
