84a0c764ad1e8d3f750aaa0da6dbcefeab4bd6a9
alinabpham <alinabpham@gmail.com>
2016-06-08 14:07:34 -0400

    Filter works

--- ice_cream/admin.py
+++ ice_cream/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Order, Flavor, Topping, Container
-#from .models import Flavor, Topping, Container
+from .models import Flavor, Topping, Container, Order
 
-model_list = [Order, Flavor, Topping, Container]
+model_list = [Flavor, Topping, Container]
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_time', 'name', 'flavor')
+    list_filter = ['order_time', 'name', 'flavor']
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(model_list)
