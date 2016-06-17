896c21bd46776771c909fb14db6825b669512672
Alexander Meyer <alexm@mighty.com>
2016-06-17 17:03:47 -0400

    Rudimentary email switch, without toppings or clean text

--- ice_cream/views.py
+++ ice_cream/views.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.utils.timezone import now
 from django.views import generic
 from .models import Flavor, Topping, Container
@@ -17,58 +18,65 @@ MODEL_TYPE_DICT = {
 }
 
 
+def get_topping_str(order):
+    toppings_str = ''
+    for topping in order.toppings.all():
+        toppings_str += str(topping) + ', '
+    toppings_str = toppings_str[:-2]
+
+    return toppings_str
+
+
+def email_to_customer(order):
+    context = {
+        'name': order.name,
+        'flavor': order.flavor,
+        'toppings': get_topping_str(order),
+        'container': order.container,
+    }
+
+    message = render_to_string('ice_cream/email_to_customer.html', context)
+
+    send_mail('Your Order',
+              message,
+              'claires.icecream.order@gmail.com',
+              [order.email],
+              fail_silently=False
+              )
+
+
+def email_to_claire(order):
+    context = {
+        'name': order.name,
+        'flavor': order.flavor,
+        'toppings': get_topping_str(order),
+        'container': order.container,
+    }
+
+    message = render_to_string('ice_cream/email_to_claire.html', context)
+
+    send_mail('New Order',
+              message,
+              'claires.icecream.order@gmail.com',
+              ['meyer.alexander.john@gmail.com'],
+              fail_silently=False
+              )
+
+
 class OrderView(generic.FormView):
     form_class = OrderForm
     success_url = 'success/'
     template_name = 'ice_cream/order.html'
 
     def form_valid(self, form):
-
-        def get_topping_str():
-            toppings_str = ''
-            for topping in form.cleaned_data.get('toppings'):
-                toppings_str += str(topping) + ', '
-            toppings_str = toppings_str[:-2]
-
-            return toppings_str
-
-        def make_order_body():
-            order_body = 'Flavor: ' + order.flavor + '\n' + \
-                         'Container: ' + order.container
-            toppings_str = get_topping_str()
-            # if not empty, show toppings
-            if len(toppings_str) > 0:
-                order_body += '\n' + 'Toppings: ' + toppings_str
-
-            return order_body
-
         order = form.save(commit=False)
         order.order_time = now()
-        order.toppings = get_topping_str()
         order.save()
 
-        body = make_order_body()
+        email_to_customer(order)
+        email_to_claire(order)
 
-        claire_email_body = 'A new order for ice cream was made by ' + order.name + '.\n\n' + body
-        send_mail('New Order',
-                  claire_email_body,
-                  'claires.icecream.order@gmail.com',
-                  ['tech@mighty.com'],
-                  fail_silently=False
-                  )
-
-        customer_email_body = order.name + ',\n\n' + "Thank you for ordering from Claire's Creamery! Your order " + \
-                              "details are as follows:" + '\n\n' + body + '\n\n' + "Let us " + \
-                              "know if there are any issues, but mostly, enjoy that ice " + \
-                              "cream!\n\nYour sweet tooth savior,\nClaire's Creamery"
-        send_mail('Your Order',
-                  customer_email_body,
-                  'claires.icecream.order@gmail.com',
-                  [order.email],
-                  fail_silently=False
-                  )
-
-        return super(OrderView, self).form_valid(form)
+        return super().form_valid(form)
 
 
 class OptionView(generic.ListView):
